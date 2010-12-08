c66b38716418cfe16fc0a0a8878961488cf0f574
Chris Cormack <chris@cormack.me.uk>
2010-12-08 19:46:18 +0000

    Added a widget to the control panel to show if a DB update is available.
    git-svn-id: http://svn.netkernel.org/svn/nk4um@25 f5a16955-ce77-4e7e-9e67-38106d585073

---
+++ module/urn.uk.org.onegch.netkernel.nk4um.web/trunk/src/uk/org/onegch/netkernel/nk4um/admin/database/StatusInlineAccessor.java
@@ -0,0 +1,19 @@
+package uk.org.onegch.netkernel.nk4um.admin.database;
+
+import org.netkernel.layer0.nkf.INKFRequestContext;
+
+import uk.org.onegch.netkernel.layer2.HttpLayer2AccessorImpl;
+import uk.org.onegch.netkernel.layer2.HttpUtil;
+
+public class StatusInlineAccessor extends HttpLayer2AccessorImpl {
+  @Override
+  public void onGet(INKFRequestContext aContext, HttpUtil util) throws Exception {
+    aContext.setCWU("res:/uk/org/onegch/netkernel/nk4um/admin/database/");
+    
+    if (aContext.source("nk4um:db:liquibase:updateAvailable", Boolean.class)) {
+      util.issueSourceRequestAsResponse("updateInline.xml");
+    } else {
+      aContext.createResponseFrom("<div/>");
+    }
+  }
+}
